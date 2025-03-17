8a0e7b9f3eddd31e68ca700be266f1558aa1e79b
bbroad25 <bbroad25@gmail.com>
2025-03-17 13:50:34 -0400

    Create upload.js

---
+++ upload.js
@@ -0,0 +1,6 @@
+const msg = {
+  to: "bbroad25@gmail.com",
+  from: "no-reply@yourdomain.com",
+  subject: "Your Subject Here",  // Make sure this line is complete
+  text: "The body of the email here"
+};
